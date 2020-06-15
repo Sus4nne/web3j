36aed218ea242666605521c7a4d709ef2c9e1140
Marc Buma <mosbuma@bumos.nl>
2020-06-15 21:17:31 +0200

    ganache start script added

---
+++ src/main/resources/scripts/start-ganache.sh
@@ -0,0 +1,8 @@
+# https://github.com/trufflesuite/ganache-cli
+
+ganache-cli \
+--port 7545 \
+--mnemonic "arrest gain myself jeans love apology kidney violin trick modify lottery charge" \
+--account="0xeec437bd2ae68fa73a955b2d0694377078b0079f7a22e92491b5aab542d8f698, 200000000000000000000"
+# --verbose \
+# --blockTime 15
